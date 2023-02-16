8b3741371a73113357964350672ed57a89328786
Umar Faruk Balarabe <umarfarookb123@gmail.com>
2023-02-16 15:48:27 +0400

    get route for lessons

--- Documents/Middlesex/Web App/cst3145/individual2/server.js
+++ Documents/Middlesex/Web App/cst3145/individual2/server.js
@@ -1,13 +1,13 @@
 
-const express = require('express')
-const app = express()
-const mongoclient = require('mongodb').mongoclient
-const http = require('http')
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-var path = require('path')
-var fs = require('fs')
-var cors = require('cors')
+var path = require('path');
+var fs = require('fs');
+app.use(express.json());
 
 // logger middleware
 app.use((req,res, next)=>{
@@ -22,33 +22,30 @@ app.use((req,res,next)=>{
     next();
 })
 
-//to extract parameter from requesthk
-app.use(express.json())
-
 //mongo db connection
-let db;
-mongoclient.connect('mongodb+srv://ismail-kabir:Danmama785@cluster0.lpmoenk.mongodb.net/test', (err,client)=>{
-    db = client.db('coursework2')
-})
+MongoClient.connect("mongodb+srv://ismail-kabir:Danmama785@cluster0.lpmoenk.mongodb.net", (error, client) => {
+    db = client.db("coursework2");
+});
 
-//get the collection name
-app.param('collectionName', (req, res, next, collectionName) => {
-    req.collection = db.collection(collectionName)
-    return next()
-})
 
 app.get('/',(req,res,next)=>{
     res.send('Please select a collection e.g /collection/messages')
 })
 
-//retrieving the lessons
-app.get('/collection/:collectionName',(req,res,next)=>{
-    req.collection.find({}).toArray((e,results)=>{
-        if(e) return next(e)
-        res.send(results)
-    })
-})
 
+app.param("collectionName", (req, res, next, collectionName)=>{
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+
+app.get("/collection/:collectionName", (req, res, next)=>{
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next();
+        res.send(results);
+    });
+});
+/*
 //adding a post to save new order
 app.post('/collection/:collectionName',(req,res,next)=>{
     req.collection.insert(req.body,(e,results)=>{
@@ -100,7 +97,7 @@ app.get('/collection/:collectionName/search', (req, res, next) => {
 app.use(function(req,res){
     res.status(404).send("File not found")
 })
-
+*/
 app.listen(port,()=>{
     console.log("App started");
 })
